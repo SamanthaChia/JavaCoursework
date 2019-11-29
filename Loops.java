2d1f91934fa7aaa3645930181ac4ae99919f27c3
SamanthaChia <43061854+SamanthaChia@users.noreply.github.com>
2019-11-29 09:36:29 +0800

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,228 @@
+import java.util.Random;
+
+public class Loops {
+	
+			//loop until n is equal to m
+			private static void loop1() {
+				int m = 1;
+				int n = m + 2;
+				while (n > m) {
+					System.out.println("In loop.");
+					n--;
+				}
+				System.out.println("Out of loop.");
+			}
+			/*
+			* PROBLEM: the loop is infinite, once started it does not stop.
+			* DIAGNOSIS: m is decremented in the loop, meaning that m gets smaller while n stays the same.
+			  The effect that the guard is always true (n is always bigger than m), so the loop once started will not stop 
+			* CORRECTION: decrement n instead of m
+			*/
+
+
+					/*************************************************************/	
+	
+	
+	
+	
+		//loop 5 times
+		public static void loop2() {
+			int i = 0;
+			while(i < 5){
+				System.out.println("In loop "+i);
+				i++;
+			}
+			System.out.println("Out of loop");
+		}
+		/*
+		 * PROBLEM:	The loop is infinite, once it starts it will be stuck in loop 0.
+		 * DIAGNOSIS: Because without brackets after a while loop, the code will only execute the first line after the while loop.
+		 *  		  Thus being stuck in an infinite loop because right after the while loop you only print the statement without
+		 * 			  increasing the value of i.
+		 * CORRECTION: Place an opening bracket at the start of while loop and place a closing bracket right after "i++;" 
+	 	*/
+	
+	
+					/*************************************************************/	
+	
+	
+	
+	
+	
+		/*
+		  Draws a trapezoid (American English)/trapezium (British English) using asterisks, the shape has 7 lines, the top line has 5 stars, the next row 1 more (ie 6), rows increase by 1 until the bottom row has 12 stars as follows:
+		            ******
+					*******
+					********
+					*********
+					**********
+					***********
+					************			
+		*/
+		private static void loop3() {
+			int k = 7;
+			int m = 13;
+			int y = 6;
+			for (int i = 0; i < k; i++) {
+				for (int j = 0; j < m; j++) {
+					if (j<y) System.out.print("*");
+				}
+				System.out.println();
+				// y++;
+			}
+		}
+
+		private static void loop3Solution() {
+			int k = 7;
+			int m = 13;
+			int y = 6;
+			for (int i = 0; i < k; i++) {
+				for (int j = 0; j < m; j++) {
+					if (j<y+i) System.out.print("*");
+				}
+				System.out.println();
+				//y++; DELETE ME
+			}
+		}
+			/* 
+			 * PROBLEM: When the code is ran the stars per row are not increasing by 1, they repeat in rows consisting of 6
+			 * 			and in columns of 7.
+			 * DIAGNOSIS: after each row, the '*' do not increase by 1, because the y value is not increasing. 
+			 * CORRECTION: in the if statement where j < y, change it to j<y+i
+			*/		
+	
+	
+					/*************************************************************/	
+
+
+
+	
+	
+			//loop until x is equal to y
+			private static void loop4(){
+				Random r = new Random();
+				int x = 0;
+				int y = 10;
+				for (;;){
+					x = r.nextInt(y);
+					if (x==y){
+						break;
+					}
+					System.out.println("x = "+x+" and y = "+y);
+				}
+				System.out.println("out of loop");
+			}
+
+			private static void loop4Solution(){
+				Random r = new Random();
+				int x = 0;
+				int y = 10;
+				for (;;){
+					x = r.nextInt(y+1);
+					System.out.println("x = "+x+" and y = "+y);
+					if (x==y){
+						break;
+					}
+					System.out.println("x = "+x+" and y = "+y);
+				}
+				System.out.println("out of loop");
+			}
+			/*
+			 * PROBLEM: The x value never reaches 10 and it will keep looping forever.
+			 * DIAGNOSIS: The value in nextInt is the value of y, so it will select a random value from 0 to 9 and never reach 10.
+			 * 				In order to reach 10, need to increase the y value in r.nextInt(y) by 1.
+			 * CORRECTION: Instead of putting x = r.nextInt(y), change it to x=r.nextInt(y+1)
+			 */
+			  
+			  
+			  
+					/*************************************************************/	
+	
+
+
+	
+	
+		/* print the first 52 Fibonacci numbers
+		   NOTE: Fibonacci numbers start 0, 1 and after this the next number is the sum of the previous two
+		   hence 0, 1, 1 (=0+1), 2 (=1+1), 3 (=1+2), 5, 8, 13...
+		*/
+
+		public static void loop5() {
+			int fibNext = 0;
+			int fib1 = 0; 
+			int fib2 = 1;
+			System.out.println(fib1);
+			System.out.println(fib2);
+			for(int i = 0; i <50; i++){ 
+				fibNext = fib1 + fib2;
+				System.out.println(fibNext);
+				fib1 = fib2;
+				fib2 = fibNext;	
+			}
+		}
+
+		public static void loop5Solution() {
+			long fibNext = 0;
+			long fib1 = 0; 
+			long fib2 = 1;
+			System.out.println(fib1);
+			System.out.println(fib2);
+			for(int i = 0; i <= 50; i++){ 
+				fibNext = fib1 + fib2;
+				System.out.println(fibNext);
+				fib1 = fib2;
+				fib2 = fibNext;	
+			}
+		}
+		/*
+		  * PROBLEM: The value printed eventually becomes negative.
+		  * DIAGNOSIS: By using int as the datatype to store the values, it has a max range of 2147483647 then any value more than 
+		  				that will be turned into a negative value.
+		  * CORRECTION: instead of using int as the datatype for fibNext, fib1 and fib2, change it to the long datatype.
+		*/
+
+		
+					/*************************************************************/	
+	
+	
+	
+		
+			//print a vertical line of 10 stars
+			public static void loop6() {
+				for (int i = 0; i < 10; i++) {
+					System.out.println("*");
+				}
+			}
+
+			/*
+			 * PROBLEM: The end results only printed one star 
+			 * DIAGNOSIS: the semicolon is outside the for loop, it being outside will just run the loop once and end it there.
+			 * CORRECTION: remove the semicolon after the for loop.
+			 */
+			 
+			 
+					/*************************************************************/	
+	
+
+
+			 
+	
+		public static void runLoops() {
+			loop1();
+			System.out.println();
+			loop2();
+			System.out.println();
+			loop3Solution();
+			System.out.println();
+			loop4Solution();
+			System.out.println();
+			loop5Solution();
+			System.out.println();
+			loop6();
+			System.out.println();
+		}
+	
+		public static void main(String[] args) {
+			runLoops();
+	}
+}
---
+++ ReceiptGenerator.java
@@ -0,0 +1,323 @@
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.TimeZone;
+
+public class ReceiptGenerator{
+    private static final String RETURNS_POLICY_URL = "http://www.FixAllPhones.co.uk/returns";
+    private static final String TERMS_AND_CONDITIONS_URL = "http://www.FixAllPhones.co.uk/terms";
+    private static final String SERVICE_REVIEW_URL = "https//uk.trustpilot.com/review/www.FixAllPhones.co.uk";
+    private static final String REFERRAL_URL = "http://www.FixAllPhones.co.uk/phonefriend";
+    private static final String SOCIAL_MEDIA = "FaceBook and Twitter";
+    private static final String COMPANY_SLOGAN = "FixAllPhones - repairs you can trust";
+    private static final int VAT_NUMBER = 123456789;
+    //static final variables are constants, they cannot be changed by methods once assigned. 
+    //By convention they are written in all caps,
+    //with words separated by the underscore for readability.
+
+    private static final String[] LOCATIONS = {
+            "King's Cross",
+            "Victoria",
+            "Waterloo",
+            "Euston",
+            "Liverpool Street",
+            "London Bridge",
+            "Paddington",
+            "Marylebone"
+    };
+    
+	private static final String[] PAYMENT_TYPES = {
+			"cash",
+			"credit card",
+			"debit card",
+			"bitcoin"
+	};
+    
+    private static final String[] TEMPLATE = {
+            "%n****************************************%n* %s *%n****************************************%n%n", // + COMPANY_SLOGAN
+            "Receipt - %s%n%n", // + receipt reference no
+            "FixAll - %s%n%n", // + shop location
+            "VAT No: %d%n%n", // + vat no
+            "Served by: %s%n%n", // + staff member's first name (TODO and initial of surname?)
+            "---------------------------------------------%nYour order:%n%n",
+            "%s%n%n", // + costs breakdown
+            "Subtotal\t\t\t\t£%.2f%n", // + subtotal (TODO: make it a task to make the receipts capable of being in euros? [ie make the £/€ in the receipt configurable] necessary because the Kings Cross branch is seeing lots of European business because of being next to St Pancras International)
+            "Tax (VAT @ 20%%)\t\t\t\t£%.2f%n%n", // + tax (TODO: should tax rate also be configurable? VAT may change post Brexit)
+            "TOTAL : %d item%s\t\t\t\t£%.2f%n%n", // + number of items, s or no s (single or plural), total amount payable
+            "PAID (%s) :\t\t\t£%.2f%n%n", // + payment type (credit card/debit card/cash/bitcoin/other crypto), amount paid
+            "TO PAY :\t\t\t\t£%.2f%n%n", // + amount left to pay
+            "---------------------------------------------%n%n%n", //no additional info
+            "Exchange and returns policy - %s%n%n", // + returns policy url
+            "All repairs guaranteed for 18 months%nRepair T&Cs - %s%n%n%n", // + terms & conditions url
+            "Rate our Service%n%s%n%n%n", // + 3rd party review site url
+            "Get 20%% off your order when you refer a friend - see our web page for details%n%s%n%n", // + referral url
+            "Follow us on %s%n" // + our social media channels
+    };
+
+    private static String dateAndTime;
+    private static String shopLocation;
+    private static String staffName;
+    private static String itemisedCosts;
+    private static int numberOfItemsPurchased;
+    private static double subtotal;
+    private static double vat;
+    private static double total; 
+    private static double outstandingBalance;
+    private static double paid;
+    private static String paymentMethod;
+    private static Scanner in;
+
+    public static void create() {
+        in = new Scanner(System.in);
+        
+        dateAndTime = getDateAndTime();
+        shopLocation = getShopLocationFromUser();
+        staffName = getFromUser("your first name");
+        staffName = capitalise(staffName);
+
+        itemisedCosts = "";
+        numberOfItemsPurchased = 0;
+        subtotal = 0;
+        
+        boolean keepGoing = true;
+        //loop so that the user can enter as many items as they need to
+        while (keepGoing) {
+            //ask user to enter information about item and charges
+            String itemDescription = getFromUser("Item or service user is being charged for");
+            //TODO client wants the item description in title case (done)
+            double unitPrice = getDoubleFromUser("the unit price (without VAT) of " + itemDescription);
+            int numberOfUnits = getIntFromUser("the number of " + itemDescription);
+            //TODO make above statements a method
+
+            //update receipt information with item and charges for item
+            itemisedCosts += addItemisedCostToReceipt(itemDescription, unitPrice, numberOfUnits);
+            subtotal += (unitPrice * numberOfUnits);
+            numberOfItemsPurchased += numberOfUnits;
+            //TODO make above three statements a method
+
+            //add more items to the receipt?
+            String more = getFromUser("another item or service? Yes/No");
+            if (more.trim().toLowerCase().startsWith("n")) {
+                keepGoing = false;
+            }
+
+            //TODO make asking the user if they wish to add more items into a method
+            
+        }//end of while loop
+        
+		vat = getVat(subtotal);
+        //calculate total with vat
+        total = addVat(subtotal);
+        
+		paid = getDoubleFromUser("amount paid as a deposit by the customer"); 
+										 //TODO write a new method to get the deposit from the user. 
+        								 //enforce that the method will (1) give the user the minimum and 
+        								 //the maximum amount of the deposit (minimum is 20% of the total, 
+        								 //and maximum is the total); (2) The method will enforce that deposit 
+        								 //amounts that are too low or too high will be rejected, 
+        								 //and the user asked to enter another amount. The minimum deposit, 
+        								 //once calculated, should be rounded to the nearest int 
+        								 //(normal rounding applies), before being shown to the user.
+        
+        paymentMethod = getFromUser("deposit payment method"); //TODO validate this input. See PAYMENT_TYPES
+        //all done with user input.
+
+        //clean up
+        in.close();
+        
+        calculateOutstandingBalance(); 
+        
+        //print the formatted receipt
+        printReceipt();
+    }
+
+    private static String getDateAndTime() {
+        Calendar now = Calendar.getInstance(TimeZone.getTimeZone("Europe/London"));
+        return String.format("%1$te/%<Tb/%<tY::%<tH%<tM%<tz(%<tZ)", now);
+    }
+
+    private static String getShopLocationFromUser() {
+        System.out.println("SHOP LOCATIONS:");
+        for (int i = 0; i < LOCATIONS.length; i++) {
+            System.out.println((i+1) + ") " + LOCATIONS[i]);
+        }
+        int choice = getIntFromUser("The number that corresponds to your shop");
+        return LOCATIONS[choice-1];
+    }
+    
+    private static String capitalise(String s) {
+		if (s == null) return("");
+		s = s.trim();
+		if (s.length() < 2) return s.toUpperCase();
+		return s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase();
+    }
+    
+    
+    private static String itemDescriptionString(String itemDescription){
+         if (itemDescription == null || itemDescription.isEmpty()) {
+            return itemDescription;
+        }
+
+        itemDescription = itemDescription.trim().replaceAll(" +"," ");
+        StringBuilder sb = new StringBuilder(); //String Builder are to create mutable string objects.
+
+        boolean convert = true;
+        for (char letter : itemDescription.toCharArray()) // loop through the itemDescription
+        {
+            if (Character.isSpaceChar(letter)) //  check if it's a space
+            {
+                convert = true;
+            }
+            else if (convert) // check if convert should be changed to a Title Case
+            {
+                letter = Character.toTitleCase(letter);
+                convert = false;
+            } 
+            else 
+            {
+                letter = Character.toLowerCase(letter); // change every other character to a lower case
+            }
+            sb.append(letter);
+        }
+        String upperCaseDescription = sb.toString();
+        String[] arrOfDescription = upperCaseDescription.split(" ");
+        for( int i = 0;i < arrOfDescription.length;i++){
+            if(arrOfDescription[i].equals("A")){
+                arrOfDescription[i] = arrOfDescription[i].toLowerCase();
+            }
+            else if(arrOfDescription[i].equals("An")){
+                arrOfDescription[i] = arrOfDescription[i].toLowerCase();
+            }
+            else if(arrOfDescription[i].equals("And")){
+                arrOfDescription[i] = arrOfDescription[i].toLowerCase();
+            }
+            else if(arrOfDescription[i].equals("The")){
+                arrOfDescription[i] = arrOfDescription[i].toLowerCase();
+            }
+            else if(arrOfDescription[i].equals("Of") ){
+                arrOfDescription[i] = arrOfDescription[i].toLowerCase();
+            }
+            else if(arrOfDescription[i].equals("With")){
+                arrOfDescription[i] = arrOfDescription[i].toLowerCase();
+            }
+        }
+        String endDescriptionArrays = String.join(" ", arrOfDescription);
+        System.out.println(endDescriptionArrays);
+        return endDescriptionArrays;
+    }
+    
+    private static String addItemisedCostToReceipt(String itemDescription, double unitPrice, int numberOfUnits) {
+        //format: numberOfUnits x itemDescription @ £unitPrice each: £(unitPrice*NumberOfUnits)
+        itemDescription = itemDescriptionString(itemDescription);
+        return String.format("%d x %s @ £%.2f each\t\t£%.2f%n", numberOfUnits, itemDescription, unitPrice, unitPrice * numberOfUnits);
+    }
+
+    private static void calculateOutstandingBalance() {
+        outstandingBalance = total - paid;
+    }
+
+    private static String getFromUser(String prompt) {
+        System.out.print("Enter " + prompt + ": ");
+        return in.nextLine();
+    }
+
+    private static int getIntFromUser(String prompt) {
+        boolean keepGoing = true;
+        int i = 0;
+        while (keepGoing) {
+            System.out.print("Enter " + prompt + ": ");
+            i = in.nextInt();
+
+            in.nextLine(); //read the rest of the line that had the int.
+            //without this, running getIntFromUser() followed by getFromUser()
+            //would result in getFromUser() returning the rest of the line that the int was part of
+            //(ie probably just a newline character)
+
+            if (i < 1) { //make sure the number is more than 0
+                System.out.println("Invalid number. Number must be greater than zero.");
+            } else {
+                keepGoing = false;
+            }
+        }
+        return i;
+    }
+
+    private static double getDoubleFromUser(String prompt) {
+        boolean keepGoing = true;
+        double i = 0;
+
+        while (keepGoing) {
+            System.out.print(("Enter " + prompt + ": "));
+            i = in.nextDouble();
+
+            in.nextLine(); //read the rest of the line (probably just a newline character)
+
+            if (i <= 0) {
+                System.out.println("Invalid number. Number must be greater than zero.");
+            } else {
+                keepGoing = false;
+            }
+        }
+
+        return i;
+    }
+
+    private static String getValuesFromUser(String itemDescriptionPrompt, String unitPricePrompt, String numberPrompt) {
+        String itemDescriptionValue = getFromUser(itemDescriptionPrompt);
+        double unitPriceValue = getDoubleFromUser(unitPricePrompt + itemDescriptionValue);
+        int numberOfUnitsValue = getIntFromUser(numberPrompt + itemDescriptionValue);
+
+        String convertUnitPriceValue = Double.toString(unitPriceValue);
+        String convertNumberOfUnitsValue = Double.toString(numberOfUnitsValue);
+        // return itemDescriptionValue ,convertUnitPriceValue,convertNumberOfUnitsValue;
+        return null;
+    }
+
+    private static void printReceipt() {
+        String s = "";
+
+        for (int i = 0; i < TEMPLATE.length; i++) {
+            s += String.format(TEMPLATE[i], getTemplateData(i));
+        }
+
+        /*
+        TODO need to fix the tab spacing in the itemised costs
+        and the other bits between the dashes (the costs breakdown etc)
+         */
+        System.out.println(s);
+    }
+
+    private static Object[] getTemplateData(int line) {
+        if (line == 0) return new Object[]{ COMPANY_SLOGAN };
+        if (line == 1) return new Object[]{ dateAndTime };
+        if (line == 2) return new Object[]{ shopLocation };
+        if (line == 3) return new Object[]{ VAT_NUMBER };
+        if (line == 4) return new Object[]{ staffName };
+        if (line == 6) return new Object[]{ itemisedCosts };
+        if (line == 7) return new Object[]{ subtotal };
+        if (line == 8) return new Object[]{ vat };
+        if (line == 9) return new Object[]{ numberOfItemsPurchased, numberOfItemsPurchased > 1 ? "s" : "", total };
+        if (line == 10) return new Object[]{ paymentMethod, paid };
+        if (line == 11) return new Object[]{ outstandingBalance };
+        if (line == 13) return new Object[]{ RETURNS_POLICY_URL };
+        if (line == 14) return new Object[]{ TERMS_AND_CONDITIONS_URL };
+        if (line == 15) return new Object[]{ SERVICE_REVIEW_URL };
+        if (line == 16) return new Object[]{ REFERRAL_URL };
+        if (line == 17) return new Object[]{ SOCIAL_MEDIA };
+        else return new Object[]{ "" }; //any lines that don't need extra data
+    }
+
+    private static double addVat(double amount) {
+        double vat = getVat(amount);
+        return amount + vat;
+    }
+    
+    private static double getVat(double amount) {
+        return (amount*20.0)/100.0; //vat = 20%
+    }
+
+    public static void main(String[] args) {
+        ReceiptGenerator.create();
+    }
+}
+
